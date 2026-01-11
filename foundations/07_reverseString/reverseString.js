c1acec92f60547211772e6a5f93b0d18d8803f89
chris <cjfiel322@gmail.com>
2026-01-11 19:59:38 +0800

    Some brute force solution lmao

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  string = string;
+  let reversedString = "";
+  for (let i = string.length - 1; i > -1; i--) {
+    reversedString += string[i];
+  }
+  return reversedString;
 };
 
+reverseString("Hello there");
 // Do not edit below this line
 module.exports = reverseString;

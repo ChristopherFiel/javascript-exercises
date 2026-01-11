9180c5cf7b6c102d067488de56ee9da4b66923b4
chris <cjfiel322@gmail.com>
2026-01-11 21:47:28 +0800

    Finished excercise 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...numbers) {
+  return array.filter((item) => !numbers.includes(item));
 };
 
+removeFromArray([1, 2, 3, 4], 3, 4);
 // Do not edit below this line
 module.exports = removeFromArray;

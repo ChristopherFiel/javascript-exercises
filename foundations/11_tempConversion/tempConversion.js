7a2b6dc434562c5a1787d0073d6bfcb62ecd6324
chris <cjfiel322@gmail.com>
2026-01-17 22:52:16 +0800

    I'm so dumbed its actually the formula that is wrong wwaasdsakhflkasjfdhdskll

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature) {
+  celciusTemp = (temperature - 32) * (5 / 9);
+  return Math.round(celciusTemp * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperature) {
+  fahrenheitTemp = (temperature * 9) / 5 + 32;
+  return Math.round(fahrenheitTemp * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
